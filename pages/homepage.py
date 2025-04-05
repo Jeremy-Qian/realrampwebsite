2b3820be02ff4f20efc98d0c0046522cac6aa1c8
Jeremy-Qian <917592095@qq.com>
2025-04-05 16:12:07 +0800

    refactor(homepage): 将CSS样式从外部文件移至HTML和Python文件
    为了简化项目结构并减少外部依赖，将`homepage.css`中的样式直接嵌入到`homepage.html`和`homepage.py`中。同时移除了不必要的`@charset`声明。

--- pages/homepage.py
+++ pages/homepage.py
@@ -11,7 +11,50 @@ html_content = """
     <meta charset="UTF-8">
     <title>Home</title>
 <style type="text/css">
-@import url("homepage.css");
+    #container {
+        width: 100%;       /* 示例：限制容器宽度 */
+        margin: 2px auto; /* 居中 */
+        font-family: Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif;
+    }
+    #banner {
+        border: 2px solid #000;
+        width: 100%;      /* 继承父容器宽度 */
+        height: 100%;    /* 固定高度（或使用其他单位） */
+        text-align: center;
+        border-radius: 5px;
+        overflow: hidden; /* 隐藏超出部分 */
+    }
+    #banner img {
+        width: 100%;     /* 宽度填满 #banner */
+        height: 100%;     /* 高度填满 #banner */
+        object-fit: cover; /* 保持比例并覆盖区域（可选） */
+        border-radius: 5px;
+        border: 2px solid #000;
+    }
+    #content{
+        float: left;
+        margin-top: 2px;
+        background: linear-gradient(to bottom left, #ECFFFF, 15%, #ABFFEC, 70%, #FFFFEC);
+        height: 300px;
+        width: 74%;
+        border-radius: 5px;
+        border: 2px solid #000;
+        text-align: center;
+    }
+    #links{
+        margin-top: 2px;
+        border-radius: 5px;
+        background: linear-gradient(to bottom right, #FFFFEC, 15%, #ABFFEC, 70%, #FFFFEC);
+        float: right;
+        border: 2px solid #000;
+        text-align: center;
+        width: 24%;
+        height: 300px;
+    }
+    .link{
+        color: lightseagreen;
+        font-weight: 4000;
+    }
 </style>
 </head>
 	
