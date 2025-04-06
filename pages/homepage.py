12164921de76d52a05856e38a5869de2c57fcc30
Jeremy-Qian <917592095@qq.com>
2025-04-06 15:29:13 +0800

    feat(homepage): 更新页面配置和样式
    - 修改页面配置，添加初始侧边栏状态为展开
    - 更新内容区域和链接区域的背景颜色和样式
    - 使用绝对路径加载横幅图片

--- pages/homepage.py
+++ pages/homepage.py
@@ -2,7 +2,8 @@ import streamlit as st
 from pathlib import Path
 import base64
 
-st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
+
+st.set_page_config(page_title="Home", page_icon="material/home", layout="wide", initial_sidebar_state="expanded")
 
 # Get absolute path for the banner image
 banner_path = Path(__file__).parent.parent / "banner.gif"
@@ -31,24 +32,22 @@ st.markdown("""
 col1, col2 = st.columns([0.74, 0.24])
 
 with col1:
-    # 内容区域
     with st.container():
         st.markdown("### Content")
         st.write("Hello")
         st.markdown("""
         <style>
-        div[data-testid="stVerticalBlock"] {
-            background-color: #ECFFFF;
+        div[data-testid="stColumn"] {
+            background-color: #FFFFEC;
             border: 2px solid #000;
             border-radius: 5px;
             padding: 20px;
-            height: 300px;
+            height: 300px !important;
         }
         </style>
         """, unsafe_allow_html=True)
 
 with col2:
-    # 链接区域
     with st.container():
         st.markdown("### Links")
         st.markdown("[Login Page](https://ramper.streamlit.app)")
@@ -56,17 +55,21 @@ with col2:
         st.markdown("[Contact Us](https://contactus.streamlit.app)")
         st.markdown("""
         <style>
-        div[data-testid="stVerticalBlock"] {
+        div[data-testid="stAppViewContainer"] {
             background-color: #FFFFEC;
             border: 2px solid #000;
             border-radius: 5px;
             padding: 20px;
-            height: 300px;
+        }
+        div[data-testid="stHeader"]  {
+            background-color: #FFFFEC;
+            border: 2px solid #000;
+            border-radius: 5px;
+            padding: 20px;
         }
         </style>
         """, unsafe_allow_html=True)
-
 # 地图区域
 with st.container():
     st.markdown("### Ramp Map")
-    st.image("../banner.gif", width=300)
+    st.image(banner_path)
