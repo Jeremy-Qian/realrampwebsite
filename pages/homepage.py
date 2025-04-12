a7b94567995f75c811408427aedd6f1b7f73eb9a
Jeremy-Qian <917592095@qq.com>
2025-04-12 19:50:26 +0800

    refactor(homepage): 调整主页布局和卡片样式
    - 将主内容区域的列宽比例从0.74:0.24调整为0.70:0.24
    - 将“Trending”部分的卡片布局改为三列，并调整卡片样式，包括宽度、边距、高度和背景颜色
    - 移除了不必要的容器嵌套，简化代码结构

--- pages/homepage.py
+++ pages/homepage.py
@@ -17,6 +17,7 @@ try:
     contents = file_.read()
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
+    
     st.markdown(
         f"""
         <div style="width: 100%; margin: 0; padding: 0;">
@@ -36,41 +37,75 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 # 主内容区域
-col1, col2 = st.columns([0.74, 0.24])
+col1, col2 = st.columns([0.70, 0.24])
 
 with col1:
-    with st.container():
-        st.markdown("### Trending")
+    st.markdown("### Trending")
+    
+    
+    colcard1, colcard2, colcard3 = st.columns([0.33, 0.33, 0.33])
+    with colcard1:
         card(
             title="Bookshop",
             text="Explore our bookshop and find the perfect books for your reading needs.",
+            url="bookshop",
+            styles={
+                "card": {
+                    "border-radius": "15px",
+                    "width": "100%",  # 修改为100%填满列宽
+                    "padding": "5px",
+                    "margin": "0px",  # 移除外边距
+                    "height": "170px",
+                    "background-color": "rgba(255, 0, 25, 0.5)",
+                },
+                "title": {"font-size": "24px", "font-weight": "bold"},
+                "text": {"font-size": "16px"}
+            }
+        )
+    with colcard2:
+        card(
+            title="Ramp Day",
+            text="The date of National Ramp Day is being figured out-- Click here to find out more...",
             styles={
                 "card": {
                     "border-radius": "15px",
                     "width": "100%",
                     "padding": "5px",
-                    "margin": "5px",
-                    "height": "150px!important",
-                    "background-color": "#FFFFEC",
+                    "margin": "0",
+                    "height": "170px",
+                    "background-color": "rgba(0, 187, 255, 0.5)",
                 },
                 "title": {"font-size": "24px", "font-weight": "bold"},
-                "text": {"font-size": "16px"},
-                "fitler": {
-                    "background-color": "#FFFFEC",
-                }
+                "text": {"font-size": "16px"}
             }
         )
-        st.markdown("""
-        <style>
-        div[data-testid="stColumn"] {
-            background-color: #FFFFEC;
-            border: 2px solid #000;
-            border-radius: 5px;
-            padding: 20px;
-            height: 300px !important;
-        }
-        </style>
-        """, unsafe_allow_html=True)
+    with colcard3:
+        card(
+            title="Time Chart (beta)",
+            text="Click here for the new ramp time chart!",
+            styles={
+                "card": {
+                    "border-radius": "15px",
+                    "width": "100%",
+                    "padding": "5px",
+                    "margin": "0",
+                    "height": "170px",
+                    "background-color": "rgba(255, 200, 0, 0.5)",
+                },
+                "title": {"font-size": "24px", "font-weight": "bold"},
+                "text": {"font-size": "16px"}
+            }
+        )
+    st.markdown("""
+    <style>
+    div[data-testid="stColumn"] {
+        background-color: #FFFFEC;
+        
+        border-radius: 5px;
+        padding: 0px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 with col2:
     with st.container():
